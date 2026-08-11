c1d56debfa9a1d53939cf28cf0477c3e8cbf0cb5
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-08-11 11:14:35 +0300

    numbergreater

---
+++ numbergreater.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if i > 17:
+    print("The number is greater than 17.")
+    print("I'm in if block.")
+else:
+    print("The number is not greater than 17.")
+    print("I'm in else block.")
+print("I'm outside the if-else block.")
