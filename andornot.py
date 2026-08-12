62eca4b4fe10fd2591d33cfa0b3403b2bc33ba50
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-08-12 11:19:59 +0300

    andornot

---
+++ andornot.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("all are true")
+else:
+    print("not all are true")
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("at least one is true")
+else:
+    print("none are true")
+
+if  b > 0 or c > 0:
+    print("at least one is true")
+else:
+    print("none are true")
