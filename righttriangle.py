1c8240d2b22a92fd775d13fcbd574f5bc6bdf45e
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-09-04 13:42:55 +0300

    righttriangle

---
+++ righttriangle.py
@@ -0,0 +1,6 @@
+print ("Half pyramid pattern using numbers")
+n = int(input("Enter the number of rows: "))   
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
