4c8eef9a7a2d55e0ded1795bdd5905a853d9e5f8
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-09-12 10:32:05 +0300

    circumfrance

---
+++ circumfrance.py
@@ -0,0 +1,5 @@
+def circumference(radius):
+    pi = 3.14159
+    return 2 * pi * radius
+r = float(input("Enter the radius of the circle: "))
+print("The circumference of the circle is:", circumference(r))
