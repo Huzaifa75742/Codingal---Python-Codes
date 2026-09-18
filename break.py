21b1f888cf556a572351c35bbb89ec4e066f213e
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-09-18 13:43:30 +0300

    break

---
+++ break.py
@@ -0,0 +1,8 @@
+a = input("Enter a word: ")
+for i in a:
+    if (i == 'a'):
+        print("a is present in the word.")
+        break
+    else:
+        print("a is not present in the word.")
+        break
