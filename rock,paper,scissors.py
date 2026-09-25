52636633cca846924aa18afed4b1134a48fb7b78
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-09-25 13:56:07 +0300

    rock,paper,scissors

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -5,3 +5,15 @@ while True:
     computer_choice = random.choice(possible_choices)
     print(f"User choice: {user_choice}")
     print(f"Computer choice: {computer_choice}")
+    if user_action == computer_action:
+        print(f"Both players selected {user_choice}. It's a tie!")
+    elif user_choice == "rock":
+        if computer_choice == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_choice == "paper":
+        if computer_choice == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
