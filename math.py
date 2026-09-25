19f2d2b4e726164b7b1cd7e113abdfbfce1d8880
Huzaifa75742 <huzaifa1usman@gmail.com>
2026-09-25 14:18:57 +0300

    math

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+print("The floor of 23.56 is", math.floor(23.56))
+print("The ceiling of 23.56 is", math.ceil(23.56))
+x = 10
+y = -15
+print("The value of x is after copying y is", math.copysign(x, y))
+
+print('The absolute value of -96 and 56 are: ' + str(math.fabs(-96)), 'and', str(math.fabs(56)))
+
+print("The GCD of 24 and 56: " + str(math.gcd(24, 56)))
--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -5,7 +5,7 @@ while True:
     computer_choice = random.choice(possible_choices)
     print(f"User choice: {user_choice}")
     print(f"Computer choice: {computer_choice}")
-    if user_action == computer_action:
+    if user_choice == computer_choice:
         print(f"Both players selected {user_choice}. It's a tie!")
     elif user_choice == "rock":
         if computer_choice == "scissors":
